07164eed1e0d0e004fde7536915e45cbee1406f5
EveLegras <ladyevemaccon@gmail.com>
2026-03-26 23:02:41 +0100

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -33,16 +33,6 @@
 			alignment: 'center'
 		});
 
-	// Gallery.
-		import lightGallery from "https://cdn.skypack.dev/lightgallery@2.0.0-beta.3";
-		
-		import lgZoom from "https://cdn.skypack.dev/lightgallery@2.0.0-beta.3/plugins/zoom";
-		
-		lightGallery(document.getElementById("gallery-container"), {
-		  speed: 500,
-		  plugins: [lgZoom]
-		});
-
 	// Nav.
 
 		// Bar.
